fbed139248c213c2a06f8a1b0c9300e8b91e9a82
Matheus Calegaro <hello@matheus.me>
2023-07-03 01:05:45 -0300

    feat: implement mqtt connection and event handling

---
+++ sketch/config.h
@@ -0,0 +1,8 @@
+#define WLAN_SSID       "YOUR_SSID_HERE"
+#define WLAN_PASS       "YOU_PASSWORD_HERE"
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "YOUR_ADAFRUIT_IO_USER_HERE"
+#define AIO_KEY         "YOUR_ADAFRUIT_IO_TOKEN_HERE"
+
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,28 +1,100 @@
+#include <WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#include "config.h"
+
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Subscribe standingDeskControl = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/smart-standing-desk", MQTT_QOS_1);
+
 const int downOptoPin = 2;
 const int upOptoPin = 3;
 
-String deskState = "DOWN";
-
-void move(int pin);
-
 void setup() {
+  Serial.begin(115200);
+
   pinMode(downOptoPin, OUTPUT);
   pinMode(upOptoPin, OUTPUT);
+
+  setupWifi();
+  randomSeed(micros());
+
+  standingDeskControl.setCallback(&onEvent);
+  mqtt.subscribe(&standingDeskControl);
 }
 
 void loop() {
-  if (deskState == "DOWN") {
-    move(upOptoPin);
-    deskState = "UP";
-  } else if (deskState == "UP") {
-    move(downOptoPin);
-    deskState = "DOWN";
-  }
+  setupMQTTBroker();
 
-  delay(10000);
+  mqtt.processPackets(10000);
+
+  if (!mqtt.ping()) {
+    mqtt.disconnect();
+  }
 }
 
-void move(int pin) {
+void setupWifi() {
+  Serial.print(F("Connecting to "));
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+
+  Serial.println();
+  Serial.println(F("Wifi connected!"));
+  Serial.print(F("IP address: "));
+  Serial.println(WiFi.localIP());
+}
+
+void setupMQTTBroker() {
+  Serial.print(F("Connecting to Adafruit IO... "));
+
+  int8_t returnCode;
+
+  while ((returnCode = mqtt.connect()) != 0) {
+    switch (returnCode) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default: Serial.println(F("Connection failed")); break;
+    }
+ 
+    if (returnCode >= 0) {
+      mqtt.disconnect();
+    }
+
+    Serial.println(F("Retrying connection..."));
+    delay(5000);
+  }
+
+  Serial.println(F("Connected!"));
+}
+
+void onEvent(char *message, uint16_t len) {
+  char messageBuffer[40];
+  snprintf(messageBuffer, sizeof(messageBuffer), "Desk status is [%s], len: [%u]", message, len);
+
+  Serial.println(messageBuffer);
+
+  if (strcmp(message, "UP") == 0) {
+    Serial.println("Moving desk up...");
+    moveDesk(upOptoPin);
+  } else if (strcmp(message, "DOWN") == 0) {
+    Serial.println("Moving desk down...");
+    moveDesk(downOptoPin);
+  }
+}
+
+void moveDesk(int pin) {
   digitalWrite(pin, HIGH);
   delay(2000);
   digitalWrite(pin, LOW);
